5a0460364276e5480396b9dc48b106d2ad690d63
JRaghu842 <jraghavendra842@gmail.com>
2023-05-06 14:28:20 +0530

    protfolio design done

---
+++ index.js
@@ -0,0 +1,12 @@
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navlist.classList.toggle("open");
+};
+
+window.onscroll = () => {
+  menu.classList.remove("bx-x");
+  navlist.classList.remove("open");
+};
